127d588cbb7cdf049fd143913064bebded4617cb
allen <allen@localhost>
2020-05-25 14:58:33 +0800

    初始提交

---
+++ src/main/java/com/allen/config/AllenConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.allen.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllenConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AllenConfigServerApplication.class, args);
+	}
+
+}
